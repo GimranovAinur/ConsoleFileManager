4316bebab37b3649e457371498a1bbc117db45ce
GimranovAinur <gimranov.ainur@gmail.com>
2018-04-05 05:03:49 +0300

    Initial commit

---
+++ src/filemanager/App.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filemanager;
+
+import java.io.File;
+import java.util.Scanner;
+import javax.swing.filechooser.FileSystemView;
+
+/**
+ *
+ * @author ainur
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        
+        FileSystemView fileSystemView = FileSystemView.getFileSystemView();
+        FileManager fm = new FileManager();
+        fm.currentFolder = fileSystemView.getDefaultDirectory();
+        
+        label:
+        while(true){
+            
+            try{
+                
+                Scanner scan = new Scanner(System.in);
+                System.out.println(fm.currentFolder + ":");
+                
+                
+                String input = scan.nextLine();
+                String[] cmd = input.split(" ");
+                
+                switch(cmd[0]){
+                    
+                    case "cd":
+                        if(cmd.length == 2){
+                            fm.changeFolder(cmd[1]);
+                        }else{
+                            System.out.println("Incorrect arguments");
+                        }
+                        System.out.println("\n");
+                        break;
+                        
+                    case "list":
+                        if(cmd.length == 1){
+                            fm.list(fm.currentFolder);
+                        }else if(cmd.length == 2){
+                            fm.list(new File(cmd[1]));
+                        }else{
+                            System.out.println("Incorrect arguments count");
+                        }
+                        System.out.println("\n");
+                        break;
+                        
+                    case "newdir":
+                        if ((cmd.length > 2) && (cmd[1].equals("-c") || cmd[1].equals("-f"))){
+                            for(int i = 2; i < cmd.length; i++){
+                                fm.createDir(cmd[i], cmd[1]);
+                            }
+                        }else{
+                            System.out.println("Incorrect arguments, key existed");
+                        }
+                        break;
+                        
+                    case "newfile":
+                        if ((cmd.length > 2) && (cmd[1].equals("-c") || cmd[1].equals("-f"))){
+                            for(int i = 2; i < cmd.length; i++){
+                                fm.createFile(cmd[i], cmd[1]);
+                            }
+                        }else{
+                            System.out.println("Incorrect arguments, key existed");
+                        }
+                        break;
+                        
+                    case "move":
+                        fm.move(cmd[1], cmd[2]);
+                        
+                    case"quit":
+                        break label;
+                    
+                    default:
+                        System.out.println("Unknown command");
+                }
+                
+                
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        }
+        
+        
+    }
+    
+}
